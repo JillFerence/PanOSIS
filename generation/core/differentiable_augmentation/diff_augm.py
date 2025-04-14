86c857c59e05088bb5c6c786b79b73bef2e7b443
Jill Ference <131290306+JillFerence@users.noreply.github.com>
2025-04-14 06:08:31 -0700

    Add files via upload

---
+++ generation/core/differentiable_augmentation/diff_augm.py
@@ -0,0 +1,11 @@
+from .AugmentPipe_kornia import AugmentPipe_kornia
+
+
+class augment_pipe():
+    def __init__(self, opt):
+        self.augment_func = AugmentPipe_kornia(opt.prob_augm, opt.no_masks).to(opt.device)
+
+    def __call__(self, batch, real=True):
+        return self.augment_func(batch)
+
+
